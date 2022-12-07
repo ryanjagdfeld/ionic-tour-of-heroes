48f70bfd592a19370dfc5b3ed5ad170189133b0d
Ryan Jagdfeld <ryan@jagdfeld.com>
2022-12-07 12:45:32 -0600

    6. Get data from a server

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -29,8 +29,15 @@ export class HeroDetailComponent {
       .subscribe(hero => this.hero = hero);
   }
 
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+  }
+  
   goBack(): void {
     this.location.back();
   }
-  
+
 }
